cc8482f1b37cfb14068eeec58b36249c14b8a80c
Miguel Angel Tomairo <migueltomairo@multiplica.com>
2019-12-30 15:16:56 -0500

    Initial Commit

---
+++ material-individual-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  material-individual-test
+//
+//  Created by Miguel Tomairo on 12/30/19.
+//  Copyright © 2019 Miguel Tomairo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
